46ad131ba91484430c104d88ee756d3e2894fc1d
Hwan <74871378+hongju904@users.noreply.github.com>
2022-11-03 23:50:22 +0900

    Create 이분탐색.py

---
+++ 이분탐색.py
@@ -0,0 +1,15 @@
+right = max(lis)
+left = min(lis)
+
+while left <= right:
+  mid = (left+right) // 2
+  count = 0
+  
+  for l in lis:
+    # 구해야할 코드
+  
+  if count >= n:
+    result = mid
+    right = mid - 1
+  else:
+    lefg = mid + 1
